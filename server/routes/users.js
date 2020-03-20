afa1dd1c50ac26c729f0d6bf4fde820a0fa8ae3e
nraufu <nraufu@gmail.com>
2020-03-20 09:22:20 +0200

    ft(user): User Profile info - add a profile info controller - add a profile route
    [finishes #171894105]

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { validate } from '../middlewares/validate';
+import { verifyToken } from '../middlewares/verifyToken';
 import { User } from '../controllers/users';
 
 const router = express.Router();
@@ -7,5 +8,6 @@ const router = express.Router();
 router.post('/signup', validate.createUser, User.createUser);
 router.post('/login', validate.login, User.login);
 router.post('/reset', validate.reset, User.passwordReset);
+router.get('/profile', verifyToken, User.profileInfo);
 
 export default router;
